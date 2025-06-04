2dd4230900f3825aedc6d584d706030cf6395d2c
ngudbhav <ngudbhav05@hotmail.com>
2025-06-04 23:17:32 +0530

    fix: add API Documentation

--- lib/credentials.rb
+++ lib/credentials.rb
@@ -1,19 +1,37 @@
+# frozen_string_literal: true
+
+# This class provides a way to access application credentials in a Rails application.
+# It allows for nested keys and also checks environment variables if the key is not found in credentials.
+# The keys are expected to be in snake_case format.
 class Credentials
+  # This method allows you to retrieve a credential by its key.
+  # If the key is not found in the credentials, it will return an empty string.
+  # If the key exists in the environment variables, it will return that value instead.
+  #
+  # @param key [String] The key to retrieve from credentials or environment variables.
+  # @return [ActiveSupport::EncryptedConfiguration, String] The value associated with the key, or an empty string if not found.
   def self.get(key = "")
     new.get(key)
   end
 
+  # Instance method to retrieve a credential by its key.
+  # If the key is not found in the credentials, it will return an empty string.
+  # If the key exists in the environment variables, it will return that value instead.
+  # @param key [String] The key to retrieve from credentials or environment variables.
+  # @return [ActiveSupport::EncryptedConfiguration, String] The value associated with the key, or an empty string if not found.
   def get(key = "")
     return "" if key.blank?
     return ENV[key] if ENV.key?(key)
 
-    keys = key.split("_").map(&:downcase!)
+    keys = key.split("_").map(&:downcase)
     result = credentials
     keys.each do |inner_key|
       result = read_credentials(inner_key, result)
     end
 
     result
+  rescue NoMethodError
+    ""
   end
 
   private
--- lib/jwt_authenticate.rb
+++ lib/jwt_authenticate.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# This class provides methods to encode and decode JWT tokens.
+# It uses the Rails application's secret key base for signing the tokens.
+# The tokens can be used for user authentication, allowing users to remain logged in for a specified duration.
+# The default expiration time is set to 24 hours, but this can be adjusted as needed.
 class JwtAuthenticate
   SECRET_KEY= Rails.application.secret_key_base
   # This will logout the user from app after 24 hours.
@@ -7,11 +11,22 @@ class JwtAuthenticate
   DEFAULT_EXPIRATION = 24.hours
 
   class << self
+    # Encodes a payload into a JWT token with an expiration time.
+    # The payload should include user-specific information, such as user_id.
+    # The expiration time defaults to 24 hours from the current time.
+    #
+    # @param payload [Hash] The payload to encode in the JWT token.
+    # @param exp [Time] The expiration time for the token. Defaults to 24 hours from now.
+    # @return [String] The encoded JWT token.
     def encode(payload, exp = DEFAULT_EXPIRATION.from_now)
       payload[:exp] = exp.to_i
       JWT.encode(payload, SECRET_KEY)
     end
 
+    # Decodes a JWT token and returns the payload as a hash.
+    # If the token is invalid or cannot be decoded, it returns an empty hash.
+    # @param token [String] The JWT token to decode.
+    # @return [HashWithIndifferentAccess] The decoded payload as a hash with indifferent access.
     def decode(token)
       body = JWT.decode(token, SECRET_KEY)[0]
       HashWithIndifferentAccess.new body
