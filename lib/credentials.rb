dedff0d69e1df4c5fc3cf8aedf39786d74e92894
ngudbhav <ngudbhav05@hotmail.com>
2025-06-08 11:53:10 +0530

    fix: sidekiq<=>redis, credentials default value, and readme update

--- lib/credentials.rb
+++ lib/credentials.rb
@@ -5,21 +5,23 @@
 # The keys are expected to be in snake_case format.
 class Credentials
   # This method allows you to retrieve a credential by its key.
-  # If the key is not found in the credentials, it will return an empty string.
+  # If the key is not found in the credentials, it will return the +default_value+.
   # If the key exists in the environment variables, it will return that value instead.
   #
   # @param key [String] The key to retrieve from credentials or environment variables.
+  # @param default_value [String] The value to return if the key is not found in credentials or environment variables.
   # @return [ActiveSupport::EncryptedConfiguration, String] The value associated with the key, or an empty string if not found.
-  def self.get(key = "")
-    new.get(key)
+  def self.get(key = "", default_value = "")
+    new.get(key, default_value)
   end
 
   # Instance method to retrieve a credential by its key.
   # If the key is not found in the credentials, it will return an empty string.
   # If the key exists in the environment variables, it will return that value instead.
   # @param key [String] The key to retrieve from credentials or environment variables.
-  # @return [ActiveSupport::EncryptedConfiguration, String] The value associated with the key, or an empty string if not found.
-  def get(key = "")
+  # @param default_value [String] The value to return if the key is not found in credentials or environment variables.
+  # @return [ActiveSupport::EncryptedConfiguration, String] The value associated with the key, or the default value if not found.
+  def get(key = "", default_value)
     return "" if key.blank?
     return ENV[key] if ENV.key?(key)
 
@@ -29,9 +31,9 @@ class Credentials
       result = read_credentials(inner_key, result)
     end
 
-    result
+    result.presence || default_value
   rescue NoMethodError
-    ""
+    default_value || ""
   end
 
   private
