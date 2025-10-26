784c21978de510b751b6177e9daf40c6f4219243
ngudbhav <ngudbhav05@hotmail.com>
2025-10-26 15:29:12 +0530

    fix: Grouped Dependencies update

--- config/application.rb
+++ config/application.rb
@@ -9,13 +9,16 @@ Bundler.require(*Rails.groups)
 module RailsBoilerplate
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 8.0
+    config.load_defaults 8.1
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib_once(ignore: %w[assets tasks templates generators middleware])
     # Enable sessions for API only applications.
     config.session_store :active_record_store, key: "_rails_session"
